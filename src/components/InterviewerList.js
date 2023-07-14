7163f615f1db622ce6fb62682e2d6290c4ce05fe
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-13 21:51:27 -0700

    Define setInterviewer function
    Defined setInterviewer function in InterviewerList component so there is no need to passid as a prop to InterviewerListItem.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,21 +5,20 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList({ interviewers, interviewer, setInterviewer }) {
   
-  const InterviewerListItems = interviewers.map((interviewerItem) => (
+  const interviewerListItems = interviewers.map((interviewerItem) => (
     <InterviewerListItem
       key={interviewerItem.id}
-      id={interviewerItem.id}
       name={interviewerItem.name}
       avatar={interviewerItem.avatar}
       selected={interviewerItem.id === interviewer}
-      setInterviewer={setInterviewer}
+      setInterviewer={() => setInterviewer(interviewerItem.id)}
     />
   ));
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{InterviewerListItems}</ul>
+      <ul className="interviewers__list">{interviewerListItems}</ul>
     </section>
   );
 };
