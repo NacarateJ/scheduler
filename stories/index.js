0180a62712af7317b5641447236da2732bc7fcc4
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-13 22:00:26 -0700

    Delete id parameter
    Deleted id parameter and updated setInterviewer function.

--- stories/index.js
+++ stories/index.js
@@ -113,7 +113,6 @@ storiesOf("InterviewerListItem", module)
   ))
   .add("Clickable", () => (
     <InterviewerListItem
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
       setInterviewer={() => action("setInterviewer")(interviewer.id)}
