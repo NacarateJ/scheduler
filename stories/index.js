7eb3c32faa4d2a9860308cfc83a972f97e58eb45
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-14 00:20:33 -0700

    Write stories for Appt component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
 
 
 //////////////////////////////////////////////////////////
@@ -145,3 +146,14 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+//////////////////////////////////////////////////////////
+/// Appointment component
+//////////////////////////////////////////////////////////
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
