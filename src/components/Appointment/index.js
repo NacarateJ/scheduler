1d86075de6f24202260f23bf04eb3674a84f075d
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-14 19:27:22 -0700

    Show appointments
    Update Appt component to render SHOW component if there are interviews booked, and if there is no interviews booked it will render the Empty component.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,21 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
-export default function Appointment(props) {
-  return <article className="appointment"></article>;
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
+export default function Appointment({time, interview}) {
+  // const {student, interviewer} = interview;
+
+  const student = interview && interview.student;
+  const interviewer = interview && interview.interviewer;
+
+
+  return (
+  <article className="appointment">
+    <Header time={time}/>
+    {interview ? <Show student={student} interviewer={interviewer}/> : <Empty/>}
+  </article>);
 };
