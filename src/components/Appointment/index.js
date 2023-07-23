a1c68ce11f54872e522ae768b7a6c9194bfbb56c
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-23 15:29:52 -0700

    Add interviewers prop
    Added new prop and passed the interviewers array to Form component.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,25 +11,26 @@ import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 
-export default function Appointment({time, interview}) {
+export default function Appointment({ time, interview, interviewers }) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
 
-  const { mode, transition, back } = useVisualMode(
-    interview ? SHOW : EMPTY
-  );
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show student={interview.student} interviewer={interview.interviewer} />
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
       )}
       {mode === CREATE && (
         <Form
-          interviewers={[]}
+          interviewers={interviewers}
           onSave={(student, interviewer) => {
             // To do
           }}
