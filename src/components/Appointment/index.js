1c82b86ff4459a291e67e497beecbe8d1e8bc2d6
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-23 13:54:40 -0700

    Use useVisualMode hook to determine mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,12 +5,37 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+
+import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment({time, interview}) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
   return (
-  <article className="appointment">
-    <Header time={time}/>
-    {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty/>}
-  </article>);
+    <article className="appointment">
+      <Header time={time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={(student, interviewer) => {
+            // To do
+          }}
+          onCancel={() => back()}
+        />
+      )}
+    </article>
+  );
 };
