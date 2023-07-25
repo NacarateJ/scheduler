4bd4cfc20f30c06ed966d829a8fa8307b6564f3e
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-07-25 16:20:21 -0700

    Rename props

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Show({student, interviewer, onEdit, onDelete}) {
+export default function Show({student, interviewer, onEdit, showConfirmation}) {
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
@@ -22,7 +22,7 @@ export default function Show({student, interviewer, onEdit, onDelete}) {
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
-            onClick={onDelete}
+            onClick={showConfirmation}
           />
         </section>
       </section>
