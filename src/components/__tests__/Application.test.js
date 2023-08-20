de83c1ce9516776d1242d995794b159655d957c8
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-08-20 13:13:15 -0700

    Revert

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -150,7 +150,7 @@ describe("Application", () => {
     );
   });
 
-  xit("shows the delete error when failing to delete an existing appointment", async () => {
+  it("shows the delete error when failing to delete an existing appointment", async () => {
     axios.delete.mockRejectedValueOnce(
       new Error("Failed to delete appointment")
     );
